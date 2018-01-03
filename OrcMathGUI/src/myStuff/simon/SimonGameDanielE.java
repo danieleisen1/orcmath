350d60bd6987fac629166d81aa8550ba8dd77e6a
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-03 14:44:58 -0500

    created simon

---
+++ OrcMathGUI/src/myStuff/simon/SimonGameDanielE.java
@@ -0,0 +1,23 @@
+package myStuff.simon;
+
+import guiTeacher.GUIApplication;
+
+public class SimonGameDanielE extends GUIApplication {
+
+	public SimonGameDanielE(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
