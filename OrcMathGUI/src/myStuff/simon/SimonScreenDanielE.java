d5e9530091106d06a1cb5ff404ffb8148e6c1046
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-03 15:27:22 -0500

    asdf

--- OrcMathGUI/src/myStuff/simon/SimonScreenDanielE.java
+++ OrcMathGUI/src/myStuff/simon/SimonScreenDanielE.java
@@ -32,8 +32,48 @@ public class SimonScreenDanielE extends ClickableScreen implements Runnable {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
+		addButtons();
+		for(ButtonInterfaceDanielE b: button){ 
+		    viewObjects.add(b); 
+		}
+		progress = getProgress();
+		text = new TextLabel(130,230,300,40,"Let's play Simon!");
+		move = new ArrayList<MoveInterfaceDanielE>();
+		//add 2 moves to start
+		lastSelectedButton = -1;
+		move.add(randomMove());
+		move.add(randomMove());
+		roundNumber = 0;
+		viewObjects.add(progress);
+		viewObjects.add(text);
+	}
 
+	private MoveInterfaceDanielE randomMove() {
+		 int bIndex = (int)(Math.random()*button.length);
+		    while(bIndex == lastSelectedButton){
+		        bIndex = (int)(Math.random()*button.length);
+		    }
+		    return getMove(bIndex);
+	}
+
+	private MoveInterfaceDanielE getMove(int bIndex) {
+		return null;
+	}
+
+	/**
+	Placeholder until partner finishes implementation of ProgressInterface
+	*/
+	
+	private ProgressInterfaceDanielE getProgress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void addButtons() {
+		int numberOfButtons = 0;
+		button = new ButtonInterfaceDanielE[numberOfButtons];
+		color = new Color[]
+				//Left off at Part 2 # 2
 	}
 
 }
